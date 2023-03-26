958c47a489bbd29e5af13cb0f18d06303f7d464a
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2023-03-25 21:53:44 -0500

    add game class

--- NintendoInventory.UI/Models/Game.cs
+++ NintendoInventory.UI/Models/Game.cs
@@ -1,6 +1,14 @@
-﻿namespace NintendoInventory.UI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NintendoInventory.UI.Models
 {
     public class Game
     {
+        public int GameId { get; set; }
+        [Required]
+        public string GameTitle { get; set; } = string.Empty;
+        [Required]
+        public string GameDescription { get; set; } = string.Empty;
+        public string GameImageURL { get; set; } = string.Empty;
     }
 }
