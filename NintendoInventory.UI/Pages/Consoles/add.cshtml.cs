bde4316ac30a1269b33f0b5298802e326633828e
Cierra Citizen <citizencc@jacks.sfasu.edu>
2023-03-26 22:23:15 -0500

    working in console

---
+++ NintendoInventory.UI/Pages/Consoles/add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NintendoInventory.UI.Pages.Consoles
+{
+    public class addModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
