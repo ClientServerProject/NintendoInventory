995fa340c2234ae897ad268877c20e2b8a23e0bb
Alej-Hernandez <122489566+Alej-Hernandez@users.noreply.github.com>
2023-03-30 13:17:36 -0500

    Changed price to decimal datatype with two decimal places

--- NintendoInventory.DB/dbo/Tables/Game.sql
+++ NintendoInventory.DB/dbo/Tables/Game.sql
@@ -1,16 +1,20 @@
 ﻿CREATE TABLE [dbo].[Game] (
-    [GameID]          INT            IDENTITY (1, 1) NOT NULL,
-    [GameTitle]       VARCHAR (250)  NOT NULL,
-    [releaseDate]     DATETIME       NOT NULL,
-    [ConsoleID]       INT            NOT NULL,
-    [GameDescription] VARCHAR (2000) NOT NULL,
-    [GameImageURL]    VARCHAR (200)  NOT NULL,
-    [ESBRRatingID]    INT            NOT NULL,
-    [Price]           MONEY          NOT NULL,
+    [GameID]          INT             IDENTITY (1, 1) NOT NULL,
+    [GameTitle]       VARCHAR (250)   NOT NULL,
+    [releaseDate]     DATETIME        NOT NULL,
+    [ConsoleID]       INT             NOT NULL,
+    [GameDescription] VARCHAR (2000)  NOT NULL,
+    [GameImageURL]    VARCHAR (200)   NOT NULL,
+    [ESBRRatingID]    INT             NULL,
+    [Price]           DECIMAL (19, 2) NOT NULL,
     CONSTRAINT [PK_Game] PRIMARY KEY CLUSTERED ([GameID] ASC),
     CONSTRAINT [FK_Game_Console] FOREIGN KEY ([ConsoleID]) REFERENCES [dbo].[Console] ([ConsoleID]),
     CONSTRAINT [FK_Game_ESBRRating] FOREIGN KEY ([ESBRRatingID]) REFERENCES [dbo].[ESBRRating] ([ESBRRatingID])
 );
+
+
+
+
 
 
 
