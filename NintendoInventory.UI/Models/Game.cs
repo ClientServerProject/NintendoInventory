ecaa563fc77ba338bb3e03086bad801802e4ab8a
Alej-Hernandez <122489566+Alej-Hernandez@users.noreply.github.com>
2023-04-03 12:22:07 -0500

    Fixed user validation for console and games. Removed 0 from Console Price textbox in the Console add page.

--- NintendoInventory.UI/Models/Game.cs
+++ NintendoInventory.UI/Models/Game.cs
@@ -5,15 +5,19 @@ namespace NintendoInventory.UI.Models
     public class Game
     {
         //public int GameId { get; set; }
-        //[Required(ErrorMessage = "This field is required.")]
-        public string GameTitle { get; set; } = string.Empty;
         [Required(ErrorMessage = "This field is required.")]
-        //public string GameDescription { get; set; } = string.Empty;
+        public string GameTitle { get; set; } = string.Empty;
+
         //[Required(ErrorMessage = "This field is required.")]
+        //public string GameDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "This field is required.")]
         public string GameImageURL { get; set; } = string.Empty;
         //public int ESBRRatingID { get; set; }
         //public int ConsoleID { get; set; }
         //public string ReleaseDate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "This field is required.")]
         public string Price { get; set; } = string.Empty;
         
 
