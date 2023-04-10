56147495ac5922199f884c3e35ead59a5f1a5abc
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2023-04-09 21:13:52 -0500

    added a delete class for consoles

--- NintendoInventory.UI/Pages/Consoles/delete.cshtml.cs
+++ NintendoInventory.UI/Pages/Consoles/delete.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using NintendoInventory.UI.Models;
 
 namespace NintendoInventory.UI.Pages.Consoles
 {
     public class deleteModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet(int id)
         {
+            using (SqlConnection conn = new SqlConnection(DBhelper.GetConnectionString()))
+            {
+                string sql = "DELETE FROM Console WHERE ConsoleID = @ConsoleID";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ConsoleID", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return RedirectToPage("Index");
+            }
         }
     }
 }
