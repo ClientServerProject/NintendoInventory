f47b7fd9bc7b84c9c1e50f2d904f4b85edbd26c1
Alej-Hernandez <122489566+Alej-Hernandez@users.noreply.github.com>
2023-04-15 18:22:47 -0500

    Fixed cancel button on modal

--- NintendoInventory.DB/dbo/Tables/Game.sql
+++ NintendoInventory.DB/dbo/Tables/Game.sql
@@ -1,11 +1,7 @@
 ﻿CREATE TABLE [dbo].[Game] (
     [GameID]          INT             IDENTITY (1, 1) NOT NULL,
     [GameTitle]       VARCHAR (250)   NOT NULL,
-<<<<<<< HEAD
-    [ReleaseYear]     INT        NULL,
-=======
     [ReleaseYear]     INT             NULL,
->>>>>>> 5bc1b291b29bde9b767b92ac7ae7f5e4fb394c5a
     [ConsoleID]       INT             NULL,
     [GameDescription] VARCHAR (2000)  NULL,
     [GameImageURL]    VARCHAR (200)   NOT NULL,
