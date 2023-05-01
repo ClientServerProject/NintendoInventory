e46d791759d44cdfa7fb25b94919cc60c5982b23
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2023-04-30 20:23:11 -0500

    Fixed User email and password validation

---
+++ NintendoInventory.DB/dbo/Tables/ProfileImage.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ProfileImage] (
+    [ProfileImageID]  INT           IDENTITY (1, 1) NOT NULL,
+    [ProfileImageURL] VARCHAR (200) NOT NULL,
+    CONSTRAINT [PK_ProfileImage] PRIMARY KEY CLUSTERED ([ProfileImageID] ASC),
+    CONSTRAINT [FK_ProfileImage_ProfileImage] FOREIGN KEY ([ProfileImageID]) REFERENCES [dbo].[ProfileImage] ([ProfileImageID])
+);
+
---
+++ NintendoInventory.UI/Models/ProfileImage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NintendoInventory.UI.Models
+{
+    public class ProfileImage
+    {
+        public int ProfileImageID { get; set; }
+        [Required]
+        public string ProfileImageURL { get; set; } = string.Empty;
+
+    }
+}
