2f8958a0e28b6718999544fa7b60235feb33be2a
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2023-05-03 10:58:55 -0500

    added authorization to protected pages

--- NintendoInventory.UI/Pages/Wishlist/AddConsole.cshtml.cs
+++ NintendoInventory.UI/Pages/Wishlist/AddConsole.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,6 +7,7 @@ using NintendoInventory.UI.Models;
 
 namespace NintendoInventory.UI.Pages.Wishlist
 {
+    [Authorize]
     public class AddConsoleModel : PageModel
     {
        
